07cde6d86fa4ca9c143b5842034e048338517cb4
Abderrahmane Benjelloun <benjelloun857@gamil.com>
2024-07-13 02:20:07 +0000

    fixes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,10 +3,15 @@
 Route module for the API
 """
 from os import getenv
+from typing import Tuple
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
