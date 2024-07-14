a16498b8dc15dc75cf18d87725c911046190ea8f
Abderrahmane Benjelloun <benjelloun857@gamil.com>
2024-07-14 02:16:53 +0000

    session auth test ver1.2

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,12 +2,13 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from typing import Tuple
-from api.v1.views import app_views
+
+
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
